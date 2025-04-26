c3cbba113d80aa79264a85b09caa07fa6bd510c5
greysnell <greysnell20@gmail.com>
2025-04-25 21:58:31 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ class CarWashStation:
         return round(price, 1)
 
     def wash_single_car(self, car: "Car") -> float:
-    if car.clean_mark < self.clean_power:
+     if car.clean_mark < self.clean_power:
         price = self.calculate_washing_price(car)
         car.clean_mark = self.clean_power
         return price
